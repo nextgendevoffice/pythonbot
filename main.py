486593a227d876a1021ed63cd94fde623b3fb06b
nextgendevoffice <nextgendevoffice@gmail.com>
2023-05-07 14:14:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # main.py
 from flask import Flask, request, abort
 from line_bot import handle_webhook
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
 @app.route("/callback", methods=["POST"])
 def callback():
+    logging.debug("Received a request")
     handle_webhook(request)
     return "OK", 200
 
