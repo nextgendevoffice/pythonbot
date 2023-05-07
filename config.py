b937893f5b12e71ce7daae315ced7743e8921a4b
nextgendevoffice <nextgendevoffice@gmail.com>
2023-05-07 13:27:54 +0700

    Update_KEY

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+API_KEY = '886eb5facd814d229f3ab17c41323a0d'
+LINE_CHANNEL_ACCESS_TOKEN = '8tR6Z7GkmuSg0Xwj1DGPkeBiSWKiP+D29jNlTejUh9hKVRaeWySDkdTD7D7IlIyHS2KTpq9zj92/jPT73Yo5l+mXH50HhqKQ3uCbF2Q9f9D4JRV14HxjnUP9OzGlPQlvk0Cs06+Yk5sa/e6ygJ9NJQdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_SECRET = '08c1695f0b7ecc5aba7ba36c10d0f281'
+MONGODB_CONNECTION_STRING = 'mongodb+srv://acc_football:9Xbr3Ce7Y1w85A60@db-mongodb-sgp1-20926-71f06c7a.mongo.ondigitalocean.com/football_bot?tls=true&authSource=admin&replicaSet=db-mongodb-sgp1-20926'
--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def callback():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8080)
